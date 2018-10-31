a74f10e90d0624cdf686f4f3697dc7ce08cbfc9c
Anthony Graniero <ananfro@gmail.com>
2018-10-31 11:58:58 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,16 @@
 class Dog
-  def initialize(name)
+  def name=(name)
     @name = name
   end
- 
+  
   def name
-    @name
+    @name 
+  end
+  def breed=(breed)
+    @breed = breed
   end
   
-  def name=(new_name)
-    @name = new_name
-  end
- 
+  def breed
+    @breed
+  end 
 end
